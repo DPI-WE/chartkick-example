fcc6d2e6ddf2742b27934c304be39757e2b15e8b
Ian Heraty <heratyian@gmail.com>
2023-10-10 15:00:49 -0500

    Add chartkick charts for sales data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :sales
+  resources :sales, only: [:index] do
+    collection do
+      get 'sales_by_month'
+      get 'sales_by_category'
+      get 'daily_sales_this_month'
+    end
+  end
+
   root "sales#index"
 end
