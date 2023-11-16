605b80000155bfbcc8e6492255f5152d4f8eb1fe
Ian Heraty <heratyian@gmail.com>
2023-11-16 13:33:41 -0600

    Add chartkick code lecture

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,4 +2,15 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def this_month_daily
+    # filter only sale_date for current month
+    # group by day
+    # sum amount
+    render json: Sale.where(sale_date: Date.today.beginning_of_month..Date.today.end_of_month).group_by_day(:sale_date).sum(:amount)
+  end
+
+  def amount_by_category
+    render json: Sale.group(:product_category).sum(:amount)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :sales
+  resources :sales do
+    collection do
+      get "amount_by_category"
+      get "this_month_daily"
+    end
+  end
   root "sales#index"
 end
