a02cd7823be2babe1a8ce0488fff7a4827d97b7c
Ian Heraty <heratyian@gmail.com>
2024-04-01 10:03:30 -0500

    Add csv export

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -14,4 +14,13 @@ class SalesController < ApplicationController
   def daily_sales_this_month
     render json: Sale.where(sale_date: Date.today.beginning_of_month..Date.today.end_of_month).group_by_day(:sale_date).sum(:amount)
   end
+
+  def export
+    # TODO: handle params
+    respond_to do |format|
+      format.csv do 
+        send_data(Sale.to_csv(Sale.last(10)), { :filename => "my_sales.csv"})
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
       get 'sales_by_month'
       get 'sales_by_category'
       get 'daily_sales_this_month'
+      get 'export'
     end
   end
+
+  # get "sales/export",  { controller: "sales", action: "export"}
 
   root "sales#index"
 end
